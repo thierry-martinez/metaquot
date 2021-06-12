9658da5d432bdee0864fe24a203a9e0c2b67ff7c
Kate <kit.ty.kate@disroot.org>
2021-06-12 01:13:12 +0100

    Remove a warning in the tests

--- tests/for/for_test.ml
+++ tests/for/for_test.ml
@@ -6,5 +6,5 @@ let () =
   | { pstr_desc = Pstr_type (Recursive, [
         { ptype_name = { txt = "t1"; _ }; _};
         { ptype_name = { txt = "t2"; _ }; _};
-        { ptype_name = { txt = "t3"; _ }; _}])} -> ()
+        { ptype_name = { txt = "t3"; _ }; _}]); _} -> ()
   | _ -> assert false
