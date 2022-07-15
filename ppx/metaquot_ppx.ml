e19ef99bb368da0d521f6c4987791d98f9380656
Thierry Martinez <Thierry.Martinez@inria.fr>
2022-07-15 19:52:12 +0200

    Solve segfault with OCaml 5.0

--- ppx/metaquot_ppx.ml
+++ ppx/metaquot_ppx.ml
@@ -1,8 +1,11 @@
 let mapper = object (self)
   inherit Ppxlib.Ast_traverse.map as super
 
-  method! expression =
+  method! expression exp =
+    Gc.minor (); (* This seems to solve a segfault in OCaml 5.0
+      but this is weird!*)
     Metaquot.Exp.lift { expression = super#expression; pattern = super#pattern }
+      exp
 
   method! pattern =
     Metaquot.Pat.lift { expression = super#expression; pattern = super#pattern }
